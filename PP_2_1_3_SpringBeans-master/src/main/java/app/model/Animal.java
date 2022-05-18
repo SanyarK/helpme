62f133498b01232374eb565a2071d1f9ebf6ebd3
SanyarK <102152149+SanyarK@users.noreply.github.com>
2022-05-18 14:31:47 +0500

    Add files via upload

---
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/Animal.java
@@ -0,0 +1,17 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public abstract class Animal {
+//    public Cat cat = null;
+//    public Dog dog = null;
+//
+//    public Animal() {
+//        this.cat = cat;
+//        this.dog = dog;
+//    }
+}
